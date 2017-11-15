f04e7b73aa0afa78b36070212835c316e464ed12
Deeshai <deeshaierrolescoffery@gmail.com>
2017-11-15 09:00:27 -0800

    modified user entry

--- mips1.asm
+++ mips1.asm
@@ -1,15 +1,23 @@
+.data
+userEntry_msg: .asciiz "Please enter a hexadecimal number of 8 characters or less:"
+userError_msg: .asciiz "You have entered a hexadecmial number."
+input: space 9
+
 .text
 
-la $a0, myWord
-li $a1, 9
 
+main:
+#Prompt for user entry
+la $a0, userEntry_msg    #Loads userEntry_msg in $a0
+li $v0, 4
+syscall
+
+la $a0, input            #Loads byte space into address.
+li $a1, 9				 #Allot the byte space for string
 li $v0, 8
 syscall
 
-
-li $v0, 10
-syscall
+add $t0, $zero, $a0		 #Copy the string address to register $t0
 
 
-.data
-myWord: .space 20
+
